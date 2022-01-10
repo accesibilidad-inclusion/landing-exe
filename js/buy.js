df04351ad3b0bb9702878191938f45526bc4bba2
Herbert Spencer <hspencer@ead.cl>
2022-01-10 09:13:09 -0300

    including apple books y kindle store

--- js/buy.js
+++ js/buy.js
@@ -11,6 +11,8 @@ function setup(){
     sel.parent("store-selector");
     sel.class('select-css');
     sel.option("Selecciona una tienda");
+    sel.option("Amazon Kindle Store");
+    sel.option("Apple Book Store");
     sel.option("Google Books");
     sel.option("JSTOR");
     sel.option("Scribid");
@@ -20,6 +22,12 @@ function setup(){
 
 function buy(){
     switch(sel.value()){
+        case "Amazon Kindle Store":
+            window.open("https://www.amazon.es/Expertos-por-Experiencia-investigaci%C3%B3n-inclusiva-ebook/dp/B09NWGB9P2/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=K8Q87JAWFXXM&keywords=expertos+por+experiencia&qid=1641816358&sprefix=expertos+por+experienci%2Caps%2C307&sr=8-1");
+            break;
+        case "Apple Book Store":
+            window.open("https://books.apple.com/us/book/expertos-por-experiencia/id1601247900?itsct=books_box_link&itscg=30200&ls=1");
+            break;
         case "Google Books":
             window.open("https://books.google.es/books?hl=es&lr=&id=1ahVEAAAQBAJ&oi=fnd&pg=PT5&ots=FWkAuFHnpW&sig=GLQ-xouSPmpTl9jsI2Br7HAbKPA#v=onepage&q&f=false");
             break;
