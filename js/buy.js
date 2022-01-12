ced540c88a1f41ce3443255b0ba9a3a233b1cb3c
Herbert Spencer <hspencer@ead.cl>
2022-01-12 10:54:50 -0300

    links a tiendas

--- js/buy.js
+++ js/buy.js
@@ -23,19 +23,19 @@ function setup(){
 function buy(){
     switch(sel.value()){
         case "Amazon Kindle Store":
-            window.open("https://www.amazon.es/dp/B09NWGB9P2");
+            window.open("https://www.amazon.es/dp/B09NWGB9P2", "_self");
             break;
         case "Apple Book Store":
-            window.open("https://books.apple.com/us/book/expertos-por-experiencia/id1601247900?itsct=books_box_link&itscg=30200&ls=1");
+            window.open("https://books.apple.com/us/book/expertos-por-experiencia/id1601247900?itsct=books_box_link&itscg=30200&ls=1", "_self");
             break;
         case "Google Books":
-            window.open("https://books.google.es/books?id=1ahVEAAAQBAJ&lpg=PT5&lr&hl=es&pg=PT5#v=onepage&q&f=false");
+            window.open("https://books.google.es/books?id=1ahVEAAAQBAJ&lpg=PT5&lr&hl=es&pg=PT5#v=onepage&q&f=false", "_self");
             break;
         case "JSTOR":
-            window.open("https://www.jstor.org/stable/j.ctv261rc3b?turn_away=true");
+            window.open("https://www.jstor.org/stable/j.ctv261rc3b?turn_away=true", "_self");
             break;
         case "Scribid":
-            window.open(" https://es.scribd.com/book/548390478/Expertos-por-Experiencia-El-proceso-de-investigacion-inclusiva-un-desafio-para-los-nuevos-tiempos");
+            window.open("https://es.scribd.com/book/548390478/Expertos-por-Experiencia-El-proceso-de-investigacion-inclusiva-un-desafio-para-los-nuevos-tiempos", "_self");
             break;
     }
 }
